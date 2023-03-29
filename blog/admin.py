7ad96bc85225f924c59a4ae9f42173ca04ba46fd
malinpalo <malinpalo.mp@gmail.com>
2023-03-29 20:17:56 +0000

    Create a Superuser, set up the admin panel to interact with our Recipes and Comments models and install the Summernote library

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    """For Admin to handle Recipes in admin view"""
+    summernote_fields = ('recipe_ingredients', 'how_to')
