f0da9a2b5ed59cf98e354bf0e79dc3ce21921669
malinpalo <malinpalo.mp@gmail.com>
2023-03-31 08:46:29 +0000

    Set up the first list of recipes view, templates files and static files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe
 
-# Create your views here.
+
+class List_of_Recipes(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-date_created')
+    paginated_by = 8
+    template_name = 'recipes.html'
