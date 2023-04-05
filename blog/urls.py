829f49dbbb27f2b6e5f0b4605f72491807576232
malinpalo <malinpalo.mp@gmail.com>
2023-04-05 13:28:01 +0000

    Add CrispyForms and update requirements.txt

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('recipe/', views.ListOfRecipes.as_view(), name='recipe'),
-    path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
+    path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_detail'),
 
 ]
