9b70c8d05af884b61fd372b47bf1e84805520885
malinpalo <malinpalo.mp@gmail.com>
2023-04-05 11:53:43 +0000

    Add core content to recipe_detail temlpate and create a view for it, create a urls.py file in blog and connect existing URLs in the file

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('recipe/', views.ListOfRecipes.as_view(), name='recipe'),
+    path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
 class ListOfRecipes(generic.ListView):
+    """List of recipes on the webpage, displays 6 per page"""
     template_name = 'recipe.html'
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-date_created')
     paginated_by = 6
+
+
+class RecipeDetails(View):
+    """
+    Views the recipe detail page
+    that displays all the recipe details
+    """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.order_by("date_added")
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
