940e46b2ef57360836394ca3a5a8bc8e5f9bcec4
malinpalo <malinpalo.mp@gmail.com>
2023-04-06 11:13:54 +0000

    Update RecipeDetails View with the comment form

--- blog/views.py
+++ blog/views.py
@@ -40,3 +40,33 @@ class RecipeDetails(View):
                 "comment_form": CommentForm()
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.order_by("date_created")
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            comment.save()
+            messages.success(request, 'Your Comment has been Successfully Added')
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
