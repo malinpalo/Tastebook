70c7516fe8c75f5a95e72c7a152e961d30a82ffc
malinpalo <malinpalo.mp@gmail.com>
2023-04-11 06:42:09 +0000

    Save updates in  urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('post_recipe/', views.add_recipe, name='post_recipe'),
     path('edit_recipe/<recipe_id>', views.edit_recipe, name='edit_recipe'),
+    path('delete_recipe/<recipe_id>', views.delete_recipe, name='delete_recipe'),
     path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
 
 ]
